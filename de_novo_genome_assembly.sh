b60edcaf32a633689a72cf2a66c11f7e2fc96f66
Leah Shizuru <36777056+leahshizuru@users.noreply.github.com>
2022-06-30 11:13:29 -1000

    commit

--- de_novo_genome_assembly.sh
+++ de_novo_genome_assembly.sh
@@ -9,7 +9,7 @@
 	fi
 
 # cd into directory where fastq.gz files are located and employ the following to batch process on pair-end fastq.gz files
-# files had the following naming scheme: sampleID_raw_F.fastq.gz and sampleID_raw_R.fastq.gz
+# files had the following naming scheme: "sampleID_raw_F.fastq.gz" and "sampleID_raw_R.fastq.gz"
 # -j specifies the amount of cores that can be run simultaneously, however this option is currently unavailable. Script will proceed with single-core trimming.
 # brace expansion enables Trim Galore! to grab all files that are named _raw_F.fastq.gz and _raw_R.fastq.gz and apply the specified trimming parameters
 	
