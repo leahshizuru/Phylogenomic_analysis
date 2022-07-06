c584f56180124423cdca642b2273339515dee37c
Leah Shizuru <36777056+leahshizuru@users.noreply.github.com>
2022-07-06 13:54:25 -1000

    commit

--- model_selectionAnd_tree_construction.sh
+++ model_selectionAnd_tree_construction.sh
@@ -13,3 +13,9 @@ iqtree -s /Users/leahshizuru/data/Shizuru_et_al._Gblocks_alignment.fasta -p /Use
 
 # reduce impact of overestimating branch support with UFBoot -bnni; determine best number of cores for run [named iteration1]
 iqtree -s /Users/leahshizuru/data/Shizuru_et_al._Gblocks_alignment.fasta -p /Users/leahshizuru/data/Shizuru_et_al._partitions.txt -m MFP+MERGE -B 1000 -bnni -T auto
+
+#run with nexus file [iteration2]. Implement nexus file for all future runs.
+iqtree -s /Users/leahshizuru/data/final_alignment.fasta -p /Users/leahshizuru/data/Shizuru_et_al._partitions.nex -m MFP+MERGE -B 1000 -bnni -T auto
+
+#resample partitions and then sites within resampled partitions [iteration3]
+iqtree -s /Users/leahshizuru/data/final_alignment.fasta -p /Users/leahshizuru/data/Shizuru_et_al._partitions.nex -m MFP+MERGE -B 1000 -bnni --sampling GENESITE -T auto
