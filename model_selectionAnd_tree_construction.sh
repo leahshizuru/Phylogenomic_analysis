e3fff8e6f9911e7f2aa4db8ad0450bddb224b00e
Leah Shizuru <36777056+leahshizuru@users.noreply.github.com>
2022-07-07 14:23:01 -1000

    Update model_selectionAnd_tree_construction.sh

--- model_selectionAnd_tree_construction.sh
+++ model_selectionAnd_tree_construction.sh
@@ -1,26 +1,19 @@
 # IQ-TREE (for model selection & tree construction)
 
-# install Anaconda for downstream analyses (alternatively you can install IQ-TREE mannually)
+# install Anaconda (alternatively you can install IQ-TREE mannually)
 
 # Activate conda environment
 # Install IQ-TREE via Anaconda 
 conda activate
 conda install -c bioconda iqtree
-
-# Find best partition scheme followed by tree inference and ultrafast bootstrap
-
-iqtree -s /Users/leahshizuru/data/Shizuru_et_al._Gblocks_alignment.fasta -p /Users/leahshizuru/data/Shizuru_et_al._partitions.txt -m MFP+MERGE -B 1000  
-
-# reduce impact of overestimating branch support with UFBoot -bnni; determine best number of cores for run [named iteration1]
-iqtree -s /Users/leahshizuru/data/Shizuru_et_al._Gblocks_alignment.fasta -p /Users/leahshizuru/data/Shizuru_et_al._partitions.txt -m MFP+MERGE -B 1000 -bnni -T auto
-
-#run with nexus file [iteration2]. Implement nexus file for all future runs.
+  
+#find the best partition scheme, reduce impact of overestimating branch support with UFBoot -bnni; determine best number of cores for run [iteration2].
 iqtree -s /Users/leahshizuru/data/final_alignment.fasta -p /Users/leahshizuru/data/Shizuru_et_al._partitions.nex -m MFP+MERGE -B 1000 -bnni -T auto
 
 #resample partitions and then sites within resampled partitions [iteration3]
 iqtree -s /Users/leahshizuru/data/final_alignment.fasta -p /Users/leahshizuru/data/Shizuru_et_al._partitions.nex -m MFP+MERGE -B 1000 -bnni --sampling GENESITE -T auto
 
-#iteration2 and iteration3 yielded the same phylogeny
+#iteration2 and iteration3 yielded the same phylogeny**
 
 #Implement ModelFinder, tree search, SH-aLRT test and ultrafast bootstrap with 1000 replicates [iteration4]
 iqtree -s /Users/leahshizuru/data/final_alignment.fasta -p /Users/leahshizuru/data/Shizuru_et_al._partitions.nex -m MFP+MERGE -B 1000 -alrt 1000 -bnni -T auto
