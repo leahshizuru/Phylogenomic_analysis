b19eb78215b6309f1d044b6a0bb40bdf9a9d4bb7
Leah Shizuru <36777056+leahshizuru@users.noreply.github.com>
2022-07-15 13:20:52 -1000

    Update model_selectionAnd_tree_construction.sh

--- model_selectionAnd_tree_construction.sh
+++ model_selectionAnd_tree_construction.sh
@@ -10,10 +10,7 @@ conda install -c bioconda iqtree
 #find the best partition scheme, reduce impact of overestimating branch support with UFBoot -bnni; determine best number of cores for run [iteration2].
 iqtree -s /Users/leahshizuru/data/final_alignment.fasta -p /Users/leahshizuru/data/Shizuru_et_al._partitions.nex -m MFP+MERGE -B 1000 -bnni -T auto
 
-#resample partitions and then sites within resampled partitions [iteration3]
-iqtree -s /Users/leahshizuru/data/final_alignment.fasta -p /Users/leahshizuru/data/Shizuru_et_al._partitions.nex -m MFP+MERGE -B 1000 -bnni --sampling GENESITE -T auto
-
-#iteration2 and iteration3 yielded the same phylogeny**
-
-#Implement ModelFinder, tree search, SH-aLRT test and ultrafast bootstrap with 1000 replicates [iteration4]
+#Implement ModelFinder, tree search, SH-aLRT test and ultrafast bootstrap with 1000 replicates [run2]
 iqtree -s /Users/leahshizuru/data/final_alignment.fasta -p /Users/leahshizuru/data/Shizuru_et_al._partitions.nex -m MFP+MERGE -B 1000 -alrt 1000 -bnni -T auto
+
+#output from run2 was used for downstream analysis
