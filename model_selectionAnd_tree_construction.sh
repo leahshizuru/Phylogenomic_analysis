42b063c61832fe98a21050911b9ffa554edd79f5
Leah Shizuru <36777056+leahshizuru@users.noreply.github.com>
2022-07-15 15:43:02 -1000

    Update model_selectionAnd_tree_construction.sh

--- model_selectionAnd_tree_construction.sh
+++ model_selectionAnd_tree_construction.sh
@@ -7,7 +7,7 @@
 conda activate
 conda install -c bioconda iqtree
 
-#see: http://www.iqtree.org/doc/iqtree-doc.pdf for documentation
+#for documentation see: (http://www.iqtree.org/doc/iqtree-doc.pdf)
 #find the best partition scheme, reduce impact of overestimating branch support with UFBoot -bnni; determine best number of cores for run [iteration2].
 iqtree -s /Users/leahshizuru/data/final_alignment.fasta -p /Users/leahshizuru/data/Shizuru_et_al._partitions.nex -m MFP+MERGE -B 1000 -bnni -T auto
 
