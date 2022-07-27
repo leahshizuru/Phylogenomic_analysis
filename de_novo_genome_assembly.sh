bf96d5ec58cd21220a71455dd9521b846a499655
Leah Shizuru <36777056+leahshizuru@users.noreply.github.com>
2022-07-27 11:11:53 -1000

    update

--- de_novo_genome_assembly.sh
+++ de_novo_genome_assembly.sh
@@ -1,6 +1,4 @@
-# De novo genome assembly for ezRAD data (software required: Trim Galore!, SPAdes, QUAST)
-
-# before proceeding, inspect sequence data using MultiQC or FastQC 
+#!/bin/bash
 
 # Trim Galore!: 
 
@@ -10,10 +8,9 @@
 
 # cd into directory where fastq.gz files are located and employ the following to batch process on pair-end fastq.gz files
 # files had the following naming scheme: "sampleID_raw_F.fastq.gz" and "sampleID_raw_R.fastq.gz"
-# -j specifies the amount of cores that can be run simultaneously, however this option is currently unavailable. Script will proceed with single-core trimming.
 # brace expansion enables Trim Galore! to grab all files that are named _raw_F.fastq.gz and _raw_R.fastq.gz and apply the specified trimming parameters
 	
-	find -name "*_raw_F.fastq.gz" | cut -d "_" -f1 | parallel -j 30 trim_galore --illumina --paired --retain_unpaired --fastqc -o trim_galore/ {}\_raw_F.fastq.gz {}\_raw_R.fastq.gz
+	find -name "*_raw_F.fastq.gz" | cut -d "_" -f1 | parallel trim_galore --illumina --paired --retain_unpaired --fastqc -o trim_galore/ {}\_raw_F.fastq.gz {}\_raw_R.fastq.gz
 	
   	find ./ -name "*.F.fq.gz" | cut -d "." -f2 | sed 's/\///' > popslist
 
