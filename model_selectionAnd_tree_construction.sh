b4e10efa6303432f6e4fb8045ecd53580cb74fb8
Leah Shizuru <36777056+leahshizuru@users.noreply.github.com>
2023-09-13 11:46:14 -1000

    send it

--- model_selectionAnd_tree_construction.sh
+++ model_selectionAnd_tree_construction.sh
@@ -5,14 +5,11 @@
 # install Anaconda (alternatively you can install IQ-TREE mannually)
 
 # Activate conda environment
-# Install IQ-TREE via Anaconda 
+# Install IQ-TREE via Anaconda if installed skip this step
 conda activate
 conda install -c bioconda iqtree
-
-#find the best partition scheme, reduce impact of overestimating branch support with UFBoot -bnni; determine best number of cores for run [iteration2].
-iqtree -s /Users/leahshizuru/data/final_alignment.fasta -p /Users/leahshizuru/data/Shizuru_et_al._partitions.nex -m MFP+MERGE -B 1000 -bnni -T auto
 
 #Implement ModelFinder, tree search, SH-aLRT test and ultrafast bootstrap with 1000 replicates [run2]
 iqtree -s /Users/leahshizuru/data/final_alignment.fasta -p /Users/leahshizuru/data/Shizuru_et_al._partitions.nex -m MFP+MERGE -B 1000 -alrt 1000 -bnni -T auto
 
-#output from run2 was used for downstream analysis
+
